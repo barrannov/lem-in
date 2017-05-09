e5f1b4959d6317769e2e588d971b8cffca658013
abaranov <abaranov@student.unit.ua>
2017-05-09 15:14:21 +0300

    absolutely workable programm

--- src/work_with_ants/ants.c
+++ src/work_with_ants/ants.c
@@ -24,13 +24,13 @@ int add_new_ant(int number, int amount, t_links **ways)
 	return (number);
 }
 
-void move_current(int number, t_links **ways)
+int move_current(int number, t_links **ways)
 {
 	t_links *ways_temp;
 	t_links *w_temp;
 
 	ways_temp = *ways;
-	while (ways_temp->next)
+	while (ways_temp)
 	{
 		w_temp = ways_temp->way;
 		if (ways_temp->shortest == 1)
@@ -45,37 +45,38 @@ void move_current(int number, t_links **ways)
 					ft_putstr(w_temp->next->connection);
 					ft_putstr(" ");
 					w_temp->next->number_of_ant = w_temp->number_of_ant;
+					w_temp->number_of_ant = 0;
 					w_temp->busy = 0;
-					return;
-
+					return (1);
 				}
 				w_temp = w_temp->next;
 			}
 		}
 		ways_temp = ways_temp->next;
 	}
+	return (0);
 }
 
 void move_ants(t_ants **all)
 {
 	int on_way; //количество муравъев уже в пути
 	int i;
-	int test;
+	int temp;
 
-	test = 20;
 	on_way = 1;
-	while (test--)
+	while (1)
 	{
 		if (on_way <= (*all)->amount_ants)
-		{
 			on_way = add_new_ant(on_way, (*all)->amount_ants, &(*all)->ways);
-		}
 		i = 1;
 		ft_putstr("\n");
+		temp = 0;
 		while (i < on_way)
 		{
-			move_current(i, &(*all)->ways);
+			temp = move_current(i, &(*all)->ways);
 			i++;
 		}
+		if(i - 1 == (*all)->amount_ants && temp == 0)
+			return;
 	}
 }
